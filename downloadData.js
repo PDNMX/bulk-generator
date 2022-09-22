5b554552f59bc1d9a9c88ed0d2fc18109e072538
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2022-09-21 19:13:16 -0500

    fix intentos

--- downloadData.js
+++ downloadData.js
@@ -37,7 +37,7 @@ const download = async (page, dir, supplier_id, endpoint, pages) => {
     await writeFile(realfilename, page, endpoint)
       .then(res => {
         let { page, hasNextPage } = res;
-        console.log(JSON.stringify({ supplier_id, pages, page, hasNextPage }));
+        //console.log(JSON.stringify({ supplier_id, pages, page, hasNextPage }));
       })
       .catch(err => {
         console.error(JSON.stringify({ supplier_id, page, err }));
@@ -50,43 +50,46 @@ const getInfo = async endpoint => {
   const token = await getToken(endpoint);
   const query = { page: 1, pageSize: 1 };
 
-  if (token.status !== 200) {
+  /* if (token.status !== 200) {
     console.log(supplier_id, token);
     return;
-  }
+  } */
 
   const { token_type, access_token } = token.data;
   const info = await getData(url, query, token_type, access_token);
 
   if (info.status !== 200 || info.data.code || typeof info.data === 'undefined') {
-    console.log(`Error en ${supplier_id}`, JSON.stringify(info));
-    return getInfo(endpoint);
-  }
+    console.error(`Error en ${supplier_id}`, info);
+    return;
+  } else {
+    const { totalRows } = info.data.pagination;
 
-  const { totalRows } = info.data.pagination;
-
-  let num = Math.floor(totalRows / endpoint.pageSize);
-  let pages = totalRows % endpoint.pageSize ? num + 1 : num;
-  let linfo = { supplier_id, totalRows, pages };
-  console.log(linfo);
-
-  let dir = `${__dirname}/data/${supplier_id}`;
-  let step = 1;
-  const maxPage = pages + 1;
-
-  for (let page = 1; page < maxPage; page += step) {
-    let prom = [];
-    for (let index = 0; index < 5 && index + page < maxPage; index++) {
-      let filename = `data-${String(page).padStart(10, '0')}.json`;
-      let realfilename = `${dir}/${filename}`;
-
-      if (!fs.existsSync(realfilename)) {
-        prom.push(download(page + index, dir, supplier_id, endpoint, pages));
+    let num = Math.floor(totalRows / endpoint.pageSize);
+    let pages = totalRows % endpoint.pageSize ? num + 1 : num;
+    let linfo = { supplier_id, totalRows, pages };
+    console.log(linfo);
+  
+    let dir = `${__dirname}/data/${supplier_id}`;
+    let step = 1;
+    const maxPage = pages + 1;
+  
+    for (let page = 1; page < maxPage; page += step) {
+      let prom = [];
+      for (let index = 0; index < 5 && index + page < maxPage; index++) {
+        let filename = `data-${String(page).padStart(10, '0')}.json`;
+        let realfilename = `${dir}/${filename}`;
+  
+        if (!fs.existsSync(realfilename)) {
+          prom.push(download(page + index, dir, supplier_id, endpoint, pages));
+          //console.log(prom)
+        }
       }
+      await Promise.allSettled(prom);
     }
-    await Promise.all(prom);
+    console.log({ Finaliza: supplier_id });
   }
-  console.log({ Finaliza: supplier_id });
+
+
 };
 
 const init = () => {
@@ -95,6 +98,10 @@ const init = () => {
       const sup_dir = `${__dirname}/data/${e.supplier_id}`;
       if (!fs.existsSync(sup_dir)) fs.mkdirSync(sup_dir, { recursive: true });
       await getInfo(e);
+    } else {
+      console.error(`${e.supplier_id} NO tiene habilitida la descarga`)
+      console.log("%cLog Message", "color: orange");
+1
     }
   });
 };
